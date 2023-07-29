2605aee10ac99b507bc9d01abb8789941b5c8800
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-07-29 10:31:42 +0300

    dz

---
+++ src/data.js
@@ -0,0 +1,10 @@
+// data.js
+const data = [
+    { id: 1, name: 'John', age: 30, city: 'New York' },
+    { id: 2, name: 'Jane', age: 25, city: 'London' },
+    { id: 3, name: 'Bob', age: 40, city: 'Berlin' },
+    // Додайте більше об'єктів, якщо потрібно
+  ];
+  
+  export default data;
+  
